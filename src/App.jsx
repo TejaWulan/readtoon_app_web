e742428f832ccd3e223898e286dff14655a4f26a
teja.wulan <teja.wulan@primecash.co.id>
2025-12-08 14:32:36 +0700

    fixing screen width

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,24 @@
 import "./App.css";
-import { BrowserRouter , Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from "./Pages/Layout";
 import { lazy } from "react";
 import { Suspense } from "react";
-import Loading from "./components/loading/index";
+import Loading from "./components/Loading/index";
 
 // Homepage
-const Home = lazy(() => import('./Pages/Home'))
+const Home = lazy(() => import("./Pages/Home"));
 
 //SignIn
-const SignIn = lazy(() => import('./Pages/SignInPages'))
-
+const SignIn = lazy(() => import("./Pages/SignInPages"));
 
 //SignIn
-const SignUp = lazy(() => import('./Pages/SignUpPage'))
+const SignUp = lazy(() => import("./Pages/SignUpPage"));
 
 //Trend
-const Trend = lazy(() => import('./Pages/Trend'))
+const Trend = lazy(() => import("./Pages/Trend"));
 
 //Trend
-const Shop = lazy(() => import('./Pages/Shop'))
+const Shop = lazy(() => import("./Pages/Shop"));
 
 function App() {
   return (
@@ -28,15 +27,15 @@ function App() {
         <Routes>
           <Route path="/" element={<Layout />}>
             <Route index element={<Home />} />
-         <Route path="/signin" element={<SignIn />} />
-        <Route path="/signup" element={<SignUp />} />
+            <Route path="/signin" element={<SignIn />} />
+            <Route path="/signup" element={<SignUp />} />
 
             <Route path="/trend" element={<Trend />} />
             <Route path="/shop" element={<Shop />} />
           </Route>
         </Routes>
       </Suspense>
-    </BrowserRouter >
+    </BrowserRouter>
   );
 }
 
